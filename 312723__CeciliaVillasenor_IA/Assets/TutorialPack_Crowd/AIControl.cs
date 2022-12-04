e5b66bcee3dd4456299adcc4eb21a47229d0e176
Ceci Villaseñor <m312723@alunos.ipb.pt>
2022-12-04 17:00:56 -0600

    Creating a city Crowd complete
    - 2nd part

--- 312723__CeciliaVillasenor_IA/Assets/TutorialPack_Crowd/AIControl.cs
+++ 312723__CeciliaVillasenor_IA/Assets/TutorialPack_Crowd/AIControl.cs
@@ -5,12 +5,17 @@ public class AIControl : MonoBehaviour {
     GameObject[] goalLocations;
     UnityEngine.AI.NavMeshAgent character;
     Animator anim;
+    float speedMult;
     void Start() {
         goalLocations = GameObject.FindGameObjectsWithTag("goal");
         character = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
         character.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
         anim = this.GetComponent<Animator>();
+        anim.SetFloat("wOffset", Random.Range(0,1));
         anim.SetTrigger("isWalking");
+        speedMult = Random.Range(0.5f, 1.3f);
+        anim.SetFloat("speedMultiplier", speedMult);
+        character.speed *= speedMult;
     }
     void Update() {
 
