605e0535c1b8b0ef7f9e2de6797cfcda4df151ed
Ceci Villaseñor <m312723@alunos.ipb.pt>
2022-12-04 11:40:03 -0600

    Crowd Simulator
    - Moving as One
    - Creating a City Part 1
    - Buildings and Stores
    - 3rd Goal

---
+++ 312723__CeciliaVillasenor_IA/Assets/Scripts/CrowdSim_Scripts/IA_Controller.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class IA_Controller : MonoBehaviour
+{
+    public GameObject goal;
+    NavMeshAgent character;
+    void Start()
+    {
+        character = this.GetComponent<NavMeshAgent>();
+        character.SetDestination(goal.transform.position);
+    }
+}
---
+++ 312723__CeciliaVillasenor_IA/Assets/TutorialPack_Crowd/AIControl.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+public class AIControl : MonoBehaviour {
+
+    GameObject[] goalLocations;
+    UnityEngine.AI.NavMeshAgent character;
+    Animator anim;
+    void Start() {
+        goalLocations = GameObject.FindGameObjectsWithTag("goal");
+        character = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+        character.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        anim = this.GetComponent<Animator>();
+        anim.SetTrigger("isWalking");
+    }
+    void Update() {
+
+        if (character.remainingDistance < 1) {
+
+            character.SetDestination(goalLocations[Random.Range(0, goalLocations.Length)].transform.position);
+        }
+    }
+}
